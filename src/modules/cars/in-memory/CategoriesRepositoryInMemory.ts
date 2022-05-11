a0a09322020017d53d16ba4e53ff8ecbc0d5141d
Gustavo Diniz <gustavo.diniz@pixelplan.net>
2022-05-11 15:07:35 -0300

    Jest added, first test added, new repository in-memory

---
+++ src/modules/cars/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,36 @@
+import { Category } from '../entities/Category';
+import {
+    ICategoryRepositories,
+    ICreateCategoryDTO,
+} from '../repositories/ICategoryRepositories';
+
+class CategoriesRepositoryInMemory implements ICategoryRepositories {
+    categories: Category[] = [];
+
+    async findByName(name: string): Promise<Category> {
+        const category = this.categories.find(
+            (category) => category.name === name
+        );
+
+        return category;
+    }
+
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = new Category();
+
+        Object.assign(category, {
+            name,
+            description,
+        });
+
+        this.categories.push(category);
+    }
+
+    async list(): Promise<Category[]> {
+        const { categories } = this;
+
+        return categories;
+    }
+}
+
+export { CategoriesRepositoryInMemory };
